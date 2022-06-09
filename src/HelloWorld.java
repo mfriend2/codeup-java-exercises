add860329e0e1e355fe88707f9fb8bcb41efaeca
Michael Friend <michael.l.friend2@gmail.com>
2022-06-09 15:03:52 -0500

    completed exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,38 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 2;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Mike";
+        System.out.println(myString);
+
+        float myNumber = (float) 3.14;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//        System.out.println(x);
+//        System.out.println(y);
     }
 }
