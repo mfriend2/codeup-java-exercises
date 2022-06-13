b6f1f4df1146b22aefac7ca96e17b2b6de1724aa
Michael Friend <michael.l.friend2@gmail.com>
2022-06-13 10:08:07 -0500

    updated;

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -7,27 +7,33 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //            i++;
 //        }
+
 //        int x = 0;
 //        do {
 //            System.out.println(x);
 //            x += 2;
 //        } while (x <= 100);
+
 //        int x = 100;
 //        do {
 //            System.out.println(x);
 //            x -= 5;
 //        } while (x >= -10);
+
 //        long x = 2;
 //        do {
 //            System.out.println(x);
 //            x *= x;
 //        } while (x < 1000000);
+
 //        for (int x = 100; x >= -10; x -= 5) {
 //            System.out.println(x);
 //        }
+
 //        for (long x = 2; x < 1000000; x *= x) {
 //            System.out.println(x);
 //        }
+
 //        int x = 1;
 //        while (x <= 100) {
 //            if (x % 15 == 0) {
@@ -44,45 +50,47 @@ public class ControlFlowExercises {
 //            }
 //            x++;
 //        }
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println("Enter a number:");
-            int userInput = scanner.nextInt();
-            System.out.println("number |" + " squared |" + " cubed " + "\t");
-            System.out.println("-------------------------");
-            for (int x = 1; x <= userInput; x++) {
-                System.out.println(x + "\t" + "   " + "|    " + (x * x) + "\t" + " |    " + (x * x * x) + "\t");
-            }
-            System.out.println("Would you like to continue? [yes/no]");
-            String answer = scanner.next();
-            if (answer.equalsIgnoreCase("no")) {
-                break;
-            }
-        }
 
-        while (true) {
-            System.out.println("Enter a grade 0-100:");
-            int grade = scanner.nextInt();
-            if (grade <= 100 && grade >= 88) {
-                System.out.println("A");
-            }
-            if (grade <= 87 && grade >= 80) {
-                System.out.println("B");
-            }
-            if (grade <= 79 && grade >= 67) {
-                System.out.println("C");
-            }
-            if (grade <= 66 && grade >= 60) {
-                System.out.println("D");
-            }
-            if (grade <= 59) {
-                System.out.println("F");
-            }
-            System.out.println("Would you like to continue? [yes/no]");
-            String answer = scanner.next();
-            if (answer.equalsIgnoreCase("no")) {
-                break;
-            }
-        }
+        Scanner scanner = new Scanner(System.in);
+
+//        while (true) {
+//            System.out.println("Enter a number:");
+//            int userInput = scanner.nextInt();
+//            System.out.println("number |" + " squared |" + " cubed " + "\t");
+//            System.out.println("-------------------------");
+//            for (int x = 1; x <= userInput; x++) {
+//                System.out.println(x + "\t" + "   " + "|    " + (x * x) + "\t" + " |    " + (x * x * x) + "\t");
+//            }
+//            System.out.println("Would you like to continue? [yes/no]");
+//            String answer = scanner.next();
+//            if (answer.equalsIgnoreCase("no")) {
+//                break;
+//            }
+//        }
+
+//        while (true) {
+//            System.out.println("Enter a grade 0-100:");
+//            int grade = scanner.nextInt();
+//            if (grade <= 100 && grade >= 88) {
+//                System.out.println("A");
+//            }
+//            else if (grade <= 87 && grade >= 80) {
+//                System.out.println("B");
+//            }
+//            else if (grade <= 79 && grade >= 67) {
+//                System.out.println("C");
+//            }
+//            else if (grade <= 66 && grade >= 60) {
+//                System.out.println("D");
+//            }
+//            else if (grade <= 59) {
+//                System.out.println("F");
+//            }
+//            System.out.println("Would you like to continue? [yes/no]");
+//            String answer = scanner.next();
+//            if (answer.equalsIgnoreCase("no")) {
+//                break;
+//            }
+//        }
     }
 }
